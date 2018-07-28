9a019f4ca0256acf9d2861d6e204f622ba2c1c76
JISOO KIM <kimjisoo1216@naver.com>
2018-07-28 09:43:14 +0000

    재판소수정

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+for i in 1..3
+  Court.create(
+      nickname: "ID#{i}",
+      title: '흑흑',
+      content: '남자친구의 군대를 기다려야할까요',
+      series: 1,
+      category: '연애',
+      hashtag: '#고무신 #군대 #기다림 #고민',
+      start_date: '0728',
+      end_date: '0828',
+      agree_num: 66,
+      disagree_num: 88
+    )
+end
