5825fed81bf2182984ba7266cd4de1fbcb68306a
Seah <blueseaangel@likelion.org>
2018-07-28 09:54:31 +0000

    seah

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
-  def index
+  def index #메인 페이지
+  end
+  
+  def court #재판소 페이지
+  end
+  
+  def court_new #재판소 글올리기
+  end
+  
+  def court_create #재판소 글 DB 등록
   end
 end
---
+++ db/migrate/20180728070957_create_courts.rb
@@ -0,0 +1,17 @@
+class CreateCourts < ActiveRecord::Migration
+  def change
+    create_table :courts do |t|
+      t.string :nickname
+      t.string :title
+      t.text :content
+      t.integer :series
+      t.string :category
+      t.string :hashtag
+      t.string :start_date
+      t.string :end_date
+      t.integer :agree_num
+      t.integer :disagree_num
+      t.timestamps null: false
+    end
+  end
+end
