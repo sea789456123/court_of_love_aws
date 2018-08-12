894cc512e047ca85a3aa3ec1d04967440e271f11
Seah <blueseaangel@likelion.org>
2018-08-12 09:56:05 +0000

    0812_home/new,devise완료

--- db/migrate/20180728082039_create_courts.rb
+++ db/migrate/20180728082039_create_courts.rb
@@ -3,12 +3,12 @@ class CreateCourts < ActiveRecord::Migration
     create_table :courts do |t|
       t.string :nickname
       t.string :title
-      t.text :content
       t.integer :series
+      t.text :content
       t.string :category
       t.string :hashtag
-      t.string :start_date
       t.string :end_date
+      t.integer :end_hour
       t.integer :agree_num
       t.integer :disagree_num
       t.timestamps null: false
